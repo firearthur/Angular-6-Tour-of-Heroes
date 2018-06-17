e352f09b0e37ae86b27a57850d89f558fa939c58
fireArthur <thefirearthur@gmail.com>
2018-06-17 12:09:42 -0700

    [Service] creates messages service and uses it in heroes service

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import Hero from './hero';
 import { Observable, of } from 'rxjs';
-
+import MessageService from './messages.service';
 import HEROES from './mock-heroes';
 
 @Injectable({
@@ -9,8 +9,10 @@ import HEROES from './mock-heroes';
 })
 export default class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   getHeroes(): Observable <Hero[]> {
+    // TODO: add the message after fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
